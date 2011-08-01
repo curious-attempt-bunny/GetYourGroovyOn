11593839965fce159c5668e2cfa36addca8eb270
Merlyn Albery-Speyer <curious.attempt.bunny@gmail.com>
2011-08-01 08:16:29 -0700

    added example JAI code

---
+++ design-using-metaclass/src/main/groovy/org/groovycasts/designusingmetaclass/Before.groovy
@@ -0,0 +1,54 @@
+package org.groovycasts.designusingmetaclass
+
+import javax.media.jai.JAI
+import javax.media.jai.PlanarImage
+import java.awt.image.renderable.ParameterBlock
+import javax.media.jai.KernelJAI
+import javax.media.jai.operator.SubtractDescriptor
+
+PlanarImage load(String filename) {
+    return JAI.create("fileload", filename)
+}
+
+PlanarImage grayScale(PlanarImage image) {
+    ParameterBlock pb = new ParameterBlock()
+    pb.addSource(image)
+    pb.add([[ 0.114D, 0.587D, 0.299D, 0.0D ]] as double[][])
+
+    return JAI.create("bandcombine", pb, null)
+}
+
+PlanarImage edgeDetect(PlanarImage image) {
+     float[] data_h = [ 1.0F,   0.0F,   -1.0F,
+                        1.414F, 0.0F,   -1.414F,
+                        1.0F,   0.0F,   -1.0F ] as float[];
+     float[] data_v = [-1.0F,  -1.414F, -1.0F,
+                        0.0F,   0.0F,    0.0F,
+                        1.0F,   1.414F,  1.0F] as float[]
+
+     KernelJAI kern_h = new KernelJAI(3,3,data_h)
+     KernelJAI kern_v = new KernelJAI(3,3,data_v)
+
+     return JAI.create("gradientmagnitude", image,
+                                      kern_h, kern_v)
+}
+
+PlanarImage subtract(PlanarImage image1, PlanarImage image2) {
+    return SubtractDescriptor.create(image1, image2, null)
+}
+
+void save(PlanarImage image, String filename) {
+    JAI.create("filestore", image, filename)
+}
+
+PlanarImage image1 = load("image1.jpg")
+PlanarImage gray1 = grayScale(image1)
+PlanarImage edge1 = edgeDetect(gray1)
+
+PlanarImage image2 = load("image2.jpg")
+PlanarImage gray2 = grayScale(image2)
+PlanarImage edge2 = edgeDetect(gray2)
+
+PlanarImage output = subtract(edge1, edge2)
+
+save(output, "output.png")
